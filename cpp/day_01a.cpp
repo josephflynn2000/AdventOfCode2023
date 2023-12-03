ca456b2eec1453529f3a0670e6a2e882ee65a5d7
josephflynn <joseph.flynn2000@gmail.com>
2023-12-03 13:11:24 -0600

    Adding day one solution for advent of code

---
+++ cpp/day_01a.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+
+char getFirstDigitCharacter(const std::string& line) {
+    for (int i = 0; i < static_cast<int>(line.length()); i++) {
+        if (isdigit(line[i])) {
+            return  line[i];
+        }
+    }
+    return '\0';
+}
+
+char getLastDigitCharacter(const std::string& line) {
+    for (int i = static_cast<int>(line.length()) - 1; i >= 0; i--) {
+        if (isdigit(line[i])) {
+            return  line[i];
+        }
+    }
+    return '\0';
+}
+
+int getTotalPerLine(const std::string& line) {
+    char total[2];
+    total[0] = getFirstDigitCharacter(line);
+    total[1] = getLastDigitCharacter(line);
+    return std::stoi(total);
+}
+
+int getTotalFromFile(std::ifstream& inputFile) {
+    int total = 0;
+    std::string line;
+    while (getline(inputFile,line)) {
+        total += getTotalPerLine(line);
+    }
+    return total;
+}
+
+int main() {
+    std::ifstream myFile ("../input/day_1_input.txt");
+    if (myFile.is_open()) {
+        std::cout << getTotalFromFile(myFile);
+        return 1;
+    }
+    myFile.close();
+    return -1;
+}
